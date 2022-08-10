8744d0fdc92929577edd91e65f614124e99ec69e
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-08-11 00:29:17 +0300

    a

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+
+const gallery = document.querySelector('.gallery');
+const galleryItem = galleryItems
+  .map(({ preview, original, description }, index) => {
+    //
+    return `
+  <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      data-index="${index}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+  })
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
