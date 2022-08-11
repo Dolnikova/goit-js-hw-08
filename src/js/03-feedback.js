ee60a49c19312efa54e1f5da611a1ac9477effea
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-08-11 15:04:32 +0300

    fin

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+// const keyForVaults = 'feedback-form-state';
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    const objectToSave = { email: email.value, message: message.value };
+    localStorage.setItem('feedback-form-state', JSON.stringify(objectToSave));
+  }, 500)
+);
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+});
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+const storageData = load('feedback-form-state');
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+}
